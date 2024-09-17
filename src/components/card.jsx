68502e6e1b4bbedbf149bfc7c7c9d3e010c7d413
ItsFeli157 <113135550+ItsFeli157@users.noreply.github.com>
2024-09-17 18:57:53 -0300

    Update card.jsx

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,14 +1,21 @@
 import PropTypes from 'prop-types';
 import styles from './card.module.css';
 
-export function Card({ children }) {
+export function Card({title, description, assignedTo, startDate, endDate}) {
   return (
     <div className={styles.card}>
-      {children}
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <p>{assignedTo}</p>
+      <p>{startDate} to {endDate}</p>
     </div>
   );
 }
 
 Card.propTypes = {
-    children: PropTypes.node.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    assignedTo: PropTypes.string.isRequired,
+    startDate: PropTypes.string.isRequired,
+    endDate: PropTypes.string.isRequired,
 }
